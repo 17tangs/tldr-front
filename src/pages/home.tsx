4ccc13252a9890433ab62e7a3c33228fb1cb4ea4
Julia Baylon <juliabaylon01@gmail.com>
2021-04-25 00:55:06 -0700

    added summary page to home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -20,9 +20,49 @@ const button: React.CSSProperties = {
     marginLeft: '225px'
 };
 
-class Home extends React.Component<ISignupPageProps> {
+const greyButton: React.CSSProperties= {
+    backgroundColor: "#C4C4C4",
+    color: "white",
+    borderRadius: 15,
+    fontFamily: "Open Sans",
+    fontSize: 18,
+    zIndex: 2,
+    height: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 5,
+    marginTop: '5%',
+    width: 50,
+    textAlign: 'center',
+    cursor: 'pointer',
+    marginLeft: '225px' 
+}
+
+class Home extends React.Component<ISignupPageProps, {showInputField: boolean, input: string}> {
+    constructor(props) {
+        super(props);
+        this.state = {showInputField: false, input: ""};
+    }
     render()
     {
+        // if user already requested a summary...
+        if(!this.state.showInputField) {
+            return (<div>
+                <TopBar/>
+                <div style={{marginLeft: '30%', marginRight: 'auto', marginTop: '200px'}}>
+                    <div style={{fontSize: 36, fontFamily: "Rhodium Libre"}} >We shortened:</div>
+                    <div style={{minWidth: 100, marginRight: "40%", borderRadius: 15, minHeight: 50, backgroundColor: "#C4C4C4", color: "white", padding: 10, fontFamily: "Open Sans"}}>{this.state.input}</div>
+                    <div style={{fontSize: 36, fontFamily: "Rhodium Libre", marginTop: 25}} >Into...</div>
+                    <div style={{minWidth: 100, marginRight: "40%", borderRadius: 15, minHeight: 50, backgroundColor: "#C4C4C4", color: "white", padding: 10, fontFamily: "Open Sans"}}>**SUMMARY HERE**</div>
+                </div>
+                <div style={{display: "flex", marginLeft: "22%"}} className="flex-row">
+                    <div style={greyButton} onClick={() => {this.setState({showInputField: true})}}>Back</div>
+                    <div style={button} onClick={() => {this.setState({showInputField: true})}}>Save</div>
+                </div>
+            </div>);
+        }
+
+        // else return regular menu
         return (
         <div>
             <TopBar/>
@@ -42,8 +82,10 @@ class Home extends React.Component<ISignupPageProps> {
                     minHeight: '13px',
                     minWidth: '50%',
                     outline: 'none' }}
+                    onChange={e => this.setState({input: e.target.value})}
+                    value={this.state.input}
                     />
-                <div style={button}>Go!</div>
+                <div style={button} onClick={() => {this.setState({showInputField: false})}}>Go!</div>
             </div>
                 <div style={{fontFamily: 'Rhodium Libre', color: '#C4C4C4', fontSize: 24, textAlign: 'center', marginTop: 50}}>Enter a website URL or any chunk of text to summarize it!</div>
         </div>);
